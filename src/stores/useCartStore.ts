6de2783d511a3a4b1eb69e817c1ff61dbb3f6635
Anastasia Garkushina <garkushina.nastya@gmail.com>
2023-09-17 22:26:32 +0300

    Store logic

--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -1,3 +1,68 @@
 import { create } from "zustand";
 
-export const useCartStore = create((set, get) => {});
+type CartItem = {
+  id: number;
+  amount: number;
+};
+
+type ShoppingCart = {
+  cartAmount: () => number;
+  cartItems: CartItem[];
+  //openCart: () => void;
+  //closeCart: () => void;
+  increaseCartAmount: (id: number) => void;
+  decreaseCartAmount: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const useCartStore = create<ShoppingCart>((set, get) => ({
+  cartAmount: () => {
+    return get().cartItems.reduce((accum, item) => {
+      return accum + item.amount;
+    }, 0);
+  },
+  cartItems: [],
+
+  increaseCartAmount: (id: number) => {
+    if (get().cartItems.find((item) => item.id === id) == null) {
+      set({
+        cartItems: [...get().cartItems, { id, amount: 1 }],
+      });
+    } else {
+      set({
+        cartItems: get().cartItems.map((item: CartItem) => {
+          if (item.id === id) {
+            return { ...item, amount: item.amount + 1 };
+          } else {
+            return item;
+          }
+        }),
+      });
+    }
+  },
+
+  decreaseCartAmount: (id: number) => {
+    if (get().cartItems.find((item) => item.id === id)?.amount === 1) {
+      set({
+        cartItems: get().cartItems.filter((item) => item.id !== id),
+      });
+    } else {
+      set({
+        cartItems: get().cartItems.map((item: CartItem) => {
+          if (item.id === id) {
+            return { ...item, amount: item.amount - 1 };
+          } else {
+            return item;
+          }
+        }),
+      });
+    }
+  },
+
+  removeFromCart: (id: number) =>
+    set({
+      cartItems: get().cartItems.filter((item) => item.id !== id),
+    }),
+}));
+
+export default useCartStore;
